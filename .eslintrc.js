3d433492471002915ebaac8954fc27a909a4e374
Gabriel Sussumu Kato <gabrielsussumu@outlook.com>
2018-07-26 19:02:27 -0300

    eslint, babel

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: ["airbnb-base", "plugin:vue/essential"],
+    rules: {
+      "no-unused-vars": 1,
+      "import/no-extraneous-dependencies": 0,
+      "linebreak-style": 0
+    }
+  };
+  
--- webpack.common.js
+++ webpack.common.js
@@ -1,14 +1,14 @@
-const path = require("path");
-const webpack = require("webpack");
-const fs = require("fs");
-const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const path = require('path');
+const webpack = require('webpack');
+const fs = require('fs');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-var appBasePath = "./Scripts/App/";
+const appBasePath = './Scripts/App/';
 
-var jsEntries = {};
-fs.readdirSync(appBasePath).forEach(function(name) {
-  var indexFile = appBasePath + name + "/index.js";
+const jsEntries = {};
+fs.readdirSync(appBasePath).forEach((name) => {
+  const indexFile = `${appBasePath + name}/index.js`;
   if (fs.existsSync(indexFile)) {
     jsEntries[name] = indexFile;
   }
@@ -17,59 +17,60 @@ fs.readdirSync(appBasePath).forEach(function(name) {
 module.exports = {
   entry: jsEntries,
   output: {
-    path: path.resolve(__dirname, "./wwwroot/js/bundle/"),
-    publicPath: "/wwwroot/assets/",
-    filename: "[name].js"
+    path: path.resolve(__dirname, './wwwroot/js/bundle/'),
+    publicPath: '/assets/',
+    filename: '[name].js',
   },
   resolve: {
-    extensions: [".js", ".vue", ".json"],
+    extensions: ['.js', '.vue', '.json'],
     alias: {
-      vue$: "vue/dist/vue.esm.js",
-      "@": path.join(__dirname, appBasePath)
-    }
+      vue$: 'vue/dist/vue.esm.js',
+      '@': path.join(__dirname, appBasePath),
+    },
   },
   module: {
     rules: [
       {
         test: /\.vue$/,
-        loader: "vue-loader",
+        loader: 'vue-loader',
         options: {
           loaders: {
-            scss: "vue-style-loader!css-loader!sass-loader",
-            sass: "vue-style-loader!css-loader!sass-loader?indentedSyntax"
-          }
-        }
+            scss: 'vue-style-loader!css-loader!sass-loader',
+            sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
+          },
+        },
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
       },
       {
         test: /\.js$/,
-        loader: "babel-loader",
-        exclude: /node_modules/
+        exclude: /node_modules/,
+        loader: 'babel-loader',
       },
       {
         test: /\.scss$/,
-        loader: "style-loader!css-loader!sass-loader"
+        loader: 'style-loader!css-loader!sass-loader',
       },
       {
         test: /\.css$/,
-        loader: "style-loader!css-loader"
+        loader: 'style-loader!css-loader',
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
-        loader: "file-loader"
+        loader: 'file-loader',
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?\S*)?$/,
-        loader: "file-loader",
+        loader: 'file-loader',
         query: {
-          name: "[name].[ext]?[hash]"
-        }
-      }
-    ]
+          name: '[name].[ext]?[hash]',
+        },
+      },
+    ],
   },
-  plugins: [
-    new VueLoaderPlugin(),
-    new MiniCssExtractPlugin({
-      filename: "styles.css"
-    })
-  ]
+  plugins: [new VueLoaderPlugin()],
 };
